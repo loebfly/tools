4d4e45646a19369eb650f9f17641d0b949860603
Loebfly <>
2022-07-19 16:57:19 +0800

    optimize

--- map/Extend.go
+++ map/Extend.go
@@ -101,3 +101,48 @@ func (e extend) ToMapIFromObj(obj interface{}) map[string]interface{} {
 	}
 	return data
 }
+
+// Filter 字典过滤
+func (e extend) Filter(p interface{}, filter func(key string, value interface{}) bool) map[string]interface{} {
+	var result = make(map[string]interface{})
+	v := reflect.ValueOf(p)
+	switch v.Kind() {
+	case reflect.Map:
+		for _, k := range v.MapKeys() {
+			if filter(k.String(), v.MapIndex(k).Interface()) {
+				result[k.String()] = v.MapIndex(k).Interface()
+			}
+		}
+		return result
+	default:
+		return result
+	}
+}
+
+// MapValue 字典值转换
+func (e extend) MapValue(p interface{}, each func(key string, value interface{}) interface{}) map[string]interface{} {
+	var result = make(map[string]interface{})
+	v := reflect.ValueOf(p)
+	switch v.Kind() {
+	case reflect.Map:
+		for _, k := range v.MapKeys() {
+			result[k.String()] = each(k.String(), v.MapIndex(k).Interface())
+		}
+		return result
+	default:
+		return result
+	}
+}
+
+// ForEach 字典遍历
+func (e extend) ForEach(p interface{}, each func(key string, value interface{})) {
+	v := reflect.ValueOf(p)
+	switch v.Kind() {
+	case reflect.Map:
+		for _, k := range v.MapKeys() {
+			each(k.String(), v.MapIndex(k).Interface())
+		}
+	default:
+		return
+	}
+}
