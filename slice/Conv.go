3d0372367a6dfcc09416e4aa31ba861ecd29506e
Loebfly <>
2022-07-19 14:59:09 +0800

    modify slice conv

--- slice/Conv.go
+++ slice/Conv.go
@@ -31,8 +31,8 @@ func (c conv) IntToS(list []int) []string {
 	return result
 }
 
-// Float64ToI 将float64切片转换为interface切片
-func (c conv) Float64ToI(list []float64) []interface{} {
+// Int8ToI 将int8切片转换为interface切片
+func (c conv) Int8ToI(list []int8) []interface{} {
 	result := make([]interface{}, 0)
 	for _, v := range list {
 		result = append(result, v)
@@ -40,11 +40,56 @@ func (c conv) Float64ToI(list []float64) []interface{} {
 	return result
 }
 
-// Float64ToS 将float64切片转换为字符串切片
-func (c conv) Float64ToS(list []float64) []string {
+// Int8ToS 将int8切片转换为字符串切片
+func (c conv) Int8ToS(list []int8) []string {
 	result := make([]string, 0)
 	for _, v := range list {
-		result = append(result, strconv.FormatFloat(v, 'f', -1, 64))
+		result = append(result, strconv.FormatInt(int64(v), 10))
+	}
+	return result
+}
+
+// Int32ToI 将int32切片转换为interface切片
+func (c conv) Int32ToI(list []int32) []interface{} {
+	result := make([]interface{}, 0)
+	for _, v := range list {
+		result = append(result, v)
+	}
+	return result
+}
+
+// Int32ToS 将int32切片转换为字符串切片
+func (c conv) Int32ToS(list []int32) []string {
+	result := make([]string, 0)
+	for _, v := range list {
+		result = append(result, strconv.FormatInt(int64(v), 10))
+	}
+	return result
+}
+
+// Int64ToI 将int64切片转换为interface切片
+func (c conv) Int64ToI(list []int64) []interface{} {
+	result := make([]interface{}, 0)
+	for _, v := range list {
+		result = append(result, v)
+	}
+	return result
+}
+
+// Int64ToS 将int64切片转换为字符串切片
+func (c conv) Int64ToS(list []int64) []string {
+	result := make([]string, 0)
+	for _, v := range list {
+		result = append(result, strconv.FormatInt(v, 10))
+	}
+	return result
+}
+
+// BoolToI 将bool切片转换为interface切片
+func (c conv) BoolToI(list []bool) []interface{} {
+	result := make([]interface{}, 0)
+	for _, v := range list {
+		result = append(result, v)
 	}
 	return result
 }
@@ -63,6 +108,24 @@ func (c conv) Float32ToS(list []float32) []string {
 	result := make([]string, 0)
 	for _, v := range list {
 		result = append(result, strconv.FormatFloat(float64(v), 'f', -1, 32))
+	}
+	return result
+}
+
+// Float64ToI 将float64切片转换为interface切片
+func (c conv) Float64ToI(list []float64) []interface{} {
+	result := make([]interface{}, 0)
+	for _, v := range list {
+		result = append(result, v)
+	}
+	return result
+}
+
+// Float64ToS 将float64切片转换为字符串切片
+func (c conv) Float64ToS(list []float64) []string {
+	result := make([]string, 0)
+	for _, v := range list {
+		result = append(result, strconv.FormatFloat(v, 'f', -1, 64))
 	}
 	return result
 }
