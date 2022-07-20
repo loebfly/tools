8a25cce79342d33c9850216def62b07c013f1870
Loebfly <>
2022-07-20 09:31:19 +0800

    optimize

--- uuid/UUID.go
+++ uuid/UUID.go
@@ -5,8 +5,8 @@ import (
 	"strings"
 )
 
-// GetV1 基于时间和MAC地址生成的UUID, offMinus 是否去除"-"
-func (ue Enter) GetV1(offMinus bool) string {
+// NewV1 基于时间和MAC地址生成的UUID, offMinus 是否去除"-"
+func (ue Enter) NewV1(offMinus bool) string {
 	u, _ := uuid.NewV1()
 	if !offMinus {
 		return strings.ReplaceAll(u.String(), "-", "")
@@ -14,8 +14,8 @@ func (ue Enter) GetV1(offMinus bool) string {
 	return u.String()
 }
 
-// GetV4 基于随机数生成的UUID, offMinus 是否去除"-"
-func (ue Enter) GetV4(offMinus bool) string {
+// NewV4 基于随机数生成的UUID, offMinus 是否去除"-"
+func (ue Enter) NewV4(offMinus bool) string {
 	u, _ := uuid.NewV4()
 	if !offMinus {
 		return strings.ReplaceAll(u.String(), "-", "")
@@ -23,7 +23,7 @@ func (ue Enter) GetV4(offMinus bool) string {
 	return u.String()
 }
 
-// Get == GetV1 基于时间和MAC地址生成的UUID, offMinus 是否去除"-"
-func (ue Enter) Get(offMinus bool) string {
-	return ue.GetV1(offMinus)
+// New == NewV4 获取去除"-"基于随机数生成的UUID
+func (ue Enter) New() string {
+	return ue.NewV4(true)
 }
