dd98418de386754f2fc1f193c7f61b3264c242ff
Loebfly <>
2022-07-20 09:52:21 +0800

    optimize

--- map/Extend.go
+++ map/Extend.go
@@ -1,9 +1,7 @@
 package mapT
 
 import (
-	"encoding/json"
 	"reflect"
-	"strings"
 )
 
 type extend struct{}
@@ -60,36 +58,6 @@ func (e extend) GetValues(p interface{}) []interface{} {
 	default:
 		return values
 	}
-}
-
-// ToJsonFromMapI 字典转Json
-func (e extend) ToJsonFromMapI(p map[string]interface{}) string {
-	jsonStr, err := json.Marshal(p)
-	if err != nil {
-		return ""
-	}
-	return string(jsonStr)
-}
-
-// ToJsonFromMapS 字典转Json
-func (e extend) ToJsonFromMapS(p map[string]string) string {
-	jsonStr, err := json.Marshal(p)
-	if err != nil {
-		return ""
-	}
-	return string(jsonStr)
-}
-
-// ToMapIFromObj 结构体转字典
-func (e extend) ToMapIFromObj(obj interface{}) map[string]interface{} {
-	obj1 := reflect.TypeOf(obj)
-	obj2 := reflect.ValueOf(obj)
-
-	var data = make(map[string]interface{})
-	for i := 0; i < obj1.NumField(); i++ {
-		data[strings.ToLower(obj1.Field(i).Name)] = obj2.Field(i).Interface()
-	}
-	return data
 }
 
 // Filter 字典过滤
