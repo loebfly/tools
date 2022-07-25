8ea7f45a09b3cded07751aa24216f509d01a1d67
Loebfly <>
2022-07-25 11:20:02 +0800

    doc

--- doc.go
+++ doc.go
@@ -5,7 +5,6 @@
 			.Condition 条件判断
 			.Random   随机数
 		tools.String 字符串
-			无子包，直接使用
 		tools.Map 字典
 			.Extend 扩展
 			.Merge 合并
@@ -14,31 +13,22 @@
 			.Extend 扩展
 			.Merge 合并
 			.Conv 转换
-		tools.Crypto // 加密
+		tools.Crypto 加密
 			.MD5
 			.Base64
 			.AES
 			.SHA
 			.RSA
-		tools.Gin
-			无子包，直接使用
+		tools.Gin gin框架
 		tools.Json 数据转换
-			无子包，直接使用
 		tools.File 文件处理
-			无子包，直接使用
 		tools.SQL
-			无子包，直接使用
 		tools.IP
-			无子包，直接使用
 		tools.Shell
 			.Firewall 防火墙
 		 	.Nginx
 		tools.SFTP
-			无子包，直接使用
 		tools.Time 时间
-			.Fmt 格式化
-			.Calculate 计算
-		tools.UUID
-			无子包，直接使用
+		tools.UUID 唯一标识
 */
 package tools
