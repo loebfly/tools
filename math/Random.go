e77d249075e06be4a6f0d3e07e71e0a0ce813d92
loebfly <357406094@qq.com>
2022-10-14 15:46:46 +0800

    bugfix

--- math/Random.go
+++ math/Random.go
@@ -13,7 +13,7 @@ func (rm randomMath) Generate(src string, length int) string {
 	result := make([]byte, length)
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i := 0; i < length; i++ {
-		result = append(result, bytes[r.Intn(len(bytes))])
+		result[i] = bytes[r.Intn(len(bytes))]
 	}
 	return string(result)
 }
